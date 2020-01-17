54ebec42f893fac2eeb680b8b7b50f23139fd442
julie-de-vries <51965170+julie-de-vries@users.noreply.github.com>
2020-01-17 15:07:05 +0100

    Add files via upload

---
+++ SIR/src/ui/Application.java
@@ -0,0 +1,25 @@
+package ui;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Julie
+ */
+
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        EcranDeConnexion ecranDeConnexion = new EcranDeConnexion();
+        ecranDeConnexion.setVisible(true);
+    }
+    
+}
