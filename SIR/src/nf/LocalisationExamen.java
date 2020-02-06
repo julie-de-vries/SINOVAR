2e07ec516c9badc51c67ffb02e001f173d9dd3a0
Peuillon <Peuillon@DESKTOP-G8MPJ1G>
2020-02-06 09:22:41 +0100

    LocalisationExamen

---
+++ SIR/src/nf/LocalisationExamen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nf;
+
+/**
+ *
+ * @author Peuillon
+ */
+public enum LocalisationExamen {
+    cérébral,
+    cervical,
+    membre_supérieur,
+    membre_inférieur,
+    thoracique,
+    abdomino_pelvien,
+    abdominal,
+    pelvien,
+    osseux,
+    cardiaque,
+    
+    
+}
